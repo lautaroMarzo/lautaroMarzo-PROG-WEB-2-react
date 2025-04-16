04563f3bb2b9c8363d07bb065edd016f5dae05f8
sofia-0998 <sofifronte@gmail.com>
2025-04-16 20:38:13 -0300

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
 import './App.css'
-
+import { useMemo, useState } from 'react'
 
 const productos = [
-  {Nombre:"Proteina",Precio:66000},
-  {Nombre:"Creatina",Precio:78000},
-  {Nombre:"Prentreno",Precio:40000},
-  {Nombre:"Proteina",Precio:66000},
-  {Nombre:"Creatina",Precio:78000},
-  {Nombre:"Prentreno",Precio:40000},
-  {Nombre:"Proteina",Precio:66000},
-  {Nombre:"Creatina",Precio:78000},
-  {Nombre:"Prentreno",Precio:40000},
-  {Nombre:"Proteina",Precio:66000},
-  {Nombre:"Creatina",Precio:78000},
-  {Nombre:"Prentreno",Precio:40000},
   {Nombre:"Proteina",Precio:66000},
   {Nombre:"Creatina",Precio:78000},
   {Nombre:"Prentreno",Precio:40000}
 ] 
 
 function App() {
+  const [carrito, setcarrito] = useState([])
 
+  const total = useMemo(
+    ()=> carrito.reduce((acc, producto) => acc + producto.Precio * producto.cantidad, 0) ,
+    [carrito]
+  )
+  // El useMmemo es un hook que basicamente tiene un activador que se pone entre corchetes 
+  // al cambiar este valor activador ejecuta el codigo que hay adentro
+
+
+  const agregarProducto = (producto)=>{
+    const exsiste = carrito.find(item => item.Nombre === producto.Nombre)
+    if (exsiste){
+    const  nuevocarrito = carrito.map(item => {
+      if (item.Nombre === producto.Nombre){
+        return{...item, cantidad: item.cantidad + 1}
+      }
+      return item 
+    }
+    )
+    setcarrito([nuevocarrito]) 
+    }else {
+      // le agrego el producto
+      setcarrito([...carrito, {...producto, cantidad:1}])
+    } 
+  }
 
   return (
     <>
     <div>
     <h1>Mi carrito de Compras</h1>
-    <h4>Añade tu compra al Carrito</h4>
+    <h4>Total de Compras: ${total}</h4>
     <div className='container mt-4'>
       <div className='row'>
       {
@@ -34,8 +47,12 @@ function App() {
           <div className="col-md-4 mb-3" key={index}>
             <div className="card h-100">
               <div className="card-body">
-               <h5 className='card-title'>{producto.Nombre}</h5>
-               <h6 className='card-subtitle'>${producto.Precio}</h6> 
+               <h6 className='card-title'>{producto.Nombre}</h6>
+               <h4 className='card-subtitle'>${producto.Precio}</h4>
+               <button 
+               className="btn btn-primary"
+               onClick={()=>agregarProducto(producto)}
+               >Agregar</button>
               </div>
             </div>
           </div>
